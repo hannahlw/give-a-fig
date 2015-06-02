aa340988bd6b2156d9efc365e8b5a0198a6f935e
stefsic <stefsicart@gmail.com>
2015-06-02 11:01:57 -0400

    add title to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'dashboard/index'
+  root 'dashboard#index'
 
   resources :compost_sites
 
   resources :food_donations
 
   devise_for :users
-  root 'devise/sessions#new'
 
 end
