ad038430a04e50e246f307e435ea58d412ec25ff
jonathantow <jonathan.w.tow@gmail.com>
2015-06-03 09:46:14 -0400

    lat and long for compostsite added by geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602144458) do
+ActiveRecord::Schema.define(version: 20150602192827) do
 
   create_table "compost_sites", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20150602144458) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "phone"
   end
 
   create_table "users", force: :cascade do |t|
