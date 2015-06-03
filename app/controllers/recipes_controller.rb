bbf88b71fef6bcaa0813d763e582971ae380f912
Hannah <hannah.westheimer@gmail.com>
2015-06-03 18:04:19 -0400

    displayed yummly in index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,21 @@ class RecipesController < ApplicationController
       ingredients = params["ingredients"].join("+")
       call = "http://api.yummly.com/v1/api/recipes?_app_id=#{ENV['yummly_app_id']}&_app_key=#{ENV['yummly_key']}&q=#{ingredients}"
       @results = JSON.parse(Net::HTTP.get(URI.parse(call)))["matches"]
+
+      @recipe_info = []
       
-      @results.each do |recipe| 
+      @results.each do |recipe|  
         call_id = "http://api.yummly.com/v1/api/recipe/#{recipe["id"]}?_app_id=#{ENV['yummly_app_id']}&_app_key=#{ENV['yummly_key']}"
-        new_results = JSON.parse(Net::HTTP.get(URI.parse(call_id)))["source"]["sourceRecipeUrl"]
+        links = JSON.parse(Net::HTTP.get(URI.parse(call_id)))["source"]["sourceRecipeUrl"]
+        name = recipe["recipeName"]
+        image = recipe["imageUrlsBySize"]["90"]
+        ingredients = recipe["ingredients"] 
+        @recipe_info << eachrecipe = {
+            :name => name,
+            :link => links,
+            :image => image,
+            :ingredient => ingredients
+        }
       end
     end 
 
