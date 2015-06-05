6ea8b4dcdaf073958d0aaa18913e74902e0d2a1e
stefsic <stefsicart@gmail.com>
2015-06-05 10:29:30 -0400

    migrate community table and make join table for user communities

--- db/migrate/20150605140050_create_communities.rb
+++ db/migrate/20150605140050_create_communities.rb
@@ -1,7 +1,10 @@
 class CreateCommunities < ActiveRecord::Migration
   def change
     create_table :communities do |t|
-
+      t.string :name
+      t.string :pickup_site
+      t.string :tag
+    
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603172002) do
+ActiveRecord::Schema.define(version: 20150605142624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "pickup_site"
+    t.string   "tag"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "compost_sites", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +54,16 @@ ActiveRecord::Schema.define(version: 20150603172002) do
     t.float    "longitude"
   end
 
+  create_table "user_communities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "community_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "user_communities", ["community_id"], name: "index_user_communities_on_community_id", using: :btree
+  add_index "user_communities", ["user_id"], name: "index_user_communities_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -66,4 +84,6 @@ ActiveRecord::Schema.define(version: 20150603172002) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "user_communities", "communities"
+  add_foreign_key "user_communities", "users"
 end
